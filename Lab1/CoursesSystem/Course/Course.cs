268f3a67e3cfc16c60f328b89df16aeb96d72f6e
egerius <e.yanin@list.ru>
2025-11-15 13:09:49 +0300

    Add method GetStudentInfo

--- Lab1/CoursesSystem/Course/Course.cs
+++ Lab1/CoursesSystem/Course/Course.cs
@@ -6,6 +6,8 @@ public abstract class Course
     public string CourseName { get; set; }
     public string CourseCode { get; set; }
     public int Credits { get; set; }
+    public List<Student> EnrolledStudents { get; set; } = new List<Student>();
+    public List<Teacher> AssignedTeachers { get; set; } = new List<Teacher>();
 
     public Course(string courseName, string courseCode, int credits)
     {
@@ -15,4 +17,13 @@ public abstract class Course
     }
 
     public abstract void DisplayCourseInfo();
+    public List<string> GetStudentInfo()
+    {
+        List<string> studentInfos = new List<string>();
+        foreach (var student in EnrolledStudents)
+        {
+            studentInfos.Add(student.GetInfo());
+        }
+        return studentInfos;
+    }
 }
