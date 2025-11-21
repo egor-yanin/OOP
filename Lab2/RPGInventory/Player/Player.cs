29ff26cbadc45e8a7ea935159b5966fe079c2664
egerius <e.yanin@list.ru>
2025-11-21 12:42:19 +0300

    Create class Player

---
+++ Lab2/RPGInventory/Player/Player.cs
@@ -0,0 +1,14 @@
+public class Player
+{
+    public string Name { get; set; }
+    public float MaxCarryWeight { get; set; }
+    public float HP { get; set; } = 100;
+    public float Armor { get; set; } = 0;
+    public float AttackDamage { get; set; } = 10;
+
+    public Player(string name, float maxCarryWeight)
+    {
+        Name = name;
+        MaxCarryWeight = maxCarryWeight;
+    }
+}
