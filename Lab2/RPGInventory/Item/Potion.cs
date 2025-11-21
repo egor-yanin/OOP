157413214d7dc37fe550b64d7450e3484ae2df9f
egerius <e.yanin@list.ru>
2025-11-21 16:40:53 +0300

    Refactor HealPotion to construct it with PotionBuilder

--- Lab2/RPGInventory/Item/Potion.cs
+++ Lab2/RPGInventory/Item/Potion.cs
@@ -1,17 +1,9 @@
 public class HealPotion : IItem
 {
-    public string Name { get; private set; }
-    public float Weight { get; private set; }
-    public string Description { get; private set; }
+    public string Name { get; set; } = "";
+    public float Weight { get; set; }
+    public string Description { get; set; } = "";
     public float HealAmount { get; set; }
-
-    public HealPotion(string name, float weight, string description, float healAmount)
-    {
-        Name = name;
-        Weight = weight;
-        Description = description;
-        HealAmount = healAmount;
-    }
 
     public void Use(Player player)
     {
