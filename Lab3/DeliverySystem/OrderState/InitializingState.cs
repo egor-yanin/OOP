d1b6bf098211ebd9e17fde3705d7d5b8b79c294a
egerius <e.yanin@list.ru>
2025-11-27 19:23:30 +0300

    Add InitializingState implementation

---
+++ Lab3/DeliverySystem/OrderState/InitializingState.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace DeliverySystem.OrderState;
+
+public class InitializingState : IOrderState
+{
+    private Order _order;
+
+    public InitializingState(Order order)
+    {
+        _order = order;
+    }
+
+    public void CheckOrder()
+    {
+        Console.WriteLine("Order is being checked.");
+    }
+
+    public void CancelOrder()
+    {
+        Console.WriteLine("Order has been canceled.");
+    }
+
+    public void ProceedOrder()
+    {
+        _order.SetState(new PaymentState(_order));
+    }
+}
