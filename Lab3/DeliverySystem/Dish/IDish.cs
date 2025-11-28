6866e1be87c5037be9a13f4ec356c6375c5a76aa
egerius <e.yanin@list.ru>
2025-11-28 12:03:41 +0300

    Add Category, Weight and Calorage properties

--- Lab3/DeliverySystem/Dish/IDish.cs
+++ Lab3/DeliverySystem/Dish/IDish.cs
@@ -1,9 +1,12 @@
 using System;
 
-namespace DeliverySystem.Dish;
+namespace DeliverySystem;
 
 public interface IDish
 {
     string Name { get; }
+    string Category { get; }
     float Price { get; }
+    float Weight { get; }
+    float Calorage { get; }
 }
