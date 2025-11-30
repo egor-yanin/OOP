04609ba66a76e12083123b7cb922a0ef4cf5d9fb
egerius <e.yanin@list.ru>
2025-11-30 13:32:01 +0300

    Add SetCourier method; add validation and code generation

--- Lab3/DeliverySystem/OrderBuilder/DefaultOrderBuilder.cs
+++ Lab3/DeliverySystem/OrderBuilder/DefaultOrderBuilder.cs
@@ -12,6 +12,12 @@ public class DefaultOrderBuilder : IOrderBuilder
         return this;
     }
 
+    public IOrderBuilder SetCourier(Courier courier)
+    {
+        _order.Courier = courier;
+        return this;
+    }
+
     public IOrderBuilder SetAddress(string address)
     {
         _order.Address = address;
@@ -20,6 +26,10 @@ public class DefaultOrderBuilder : IOrderBuilder
 
     public IOrderBuilder SetDeliveryDate(DateTime deliveryDate)
     {
+        if (deliveryDate < DateTime.Now)
+        {
+            throw new ArgumentException("Delivery date cannot be in the past.");
+        }
         _order.DeliveryDate = deliveryDate;
         return this;
     }
@@ -38,12 +48,34 @@ public class DefaultOrderBuilder : IOrderBuilder
 
     public IOrderBuilder AddDish(IDish dish, int quantity)
     {
+        if (quantity <= 0)
+        {
+            throw new ArgumentException("Quantity must be greater than zero.");
+        }
         _order.AddDish(dish, quantity);
         return this;
     }
 
     public Order Build()
     {
+        if (string.IsNullOrEmpty(_order.Address))
+        {
+            throw new InvalidOperationException("Address is required to build an order.");
+        }
+        if (_order.Customer == null)
+        {
+            throw new InvalidOperationException("Customer is required to build an order.");
+        }
+        if (_order.Courier == null)
+        {
+            throw new InvalidOperationException("Courier is required to build an order.");
+        }
+        if (_order.GetDishes().Count == 0)
+        {
+            throw new InvalidOperationException("At least one dish is required to build an order.");
+        }
+
+        _order.Code = Guid.NewGuid().ToString().Substring(0, 8).ToUpper();
         return _order;
     }
 }
--- Lab3/DeliverySystem/OrderBuilder/IOrderBuilder.cs
+++ Lab3/DeliverySystem/OrderBuilder/IOrderBuilder.cs
@@ -5,6 +5,7 @@ namespace DeliverySystem;
 public interface IOrderBuilder
 {
     IOrderBuilder SetCustomer(User user);
+    IOrderBuilder SetCourier(Courier courier);
     IOrderBuilder SetAddress(string address);
     IOrderBuilder SetDeliveryDate(DateTime deliveryDate);
     IOrderBuilder SetPaymentMethod(IPaymentStrategy paymentStrategy);
